75f893f354eeea8d9aeae45a5e577355cb3eaf3a
Ali Caglayan <alizter@gmail.com>
2018-12-21 17:22:03 +0000

    modified script

--- mpi/test.c
+++ mpi/test.c
@@ -3,32 +3,44 @@
 
 int main(int argc, char **argv)
 {
-	int rc, myrank, nproc, namelen;
-	char name[MPI_MAX_PROCESSOR_NAME];
-	rc = MPI_Init(&argc, &argv);
-	
-	if (rc != MPI_SUCCESS) 
-	{
-		printf ("Error starting MPI program\n");
-		MPI_Abort(MPI_COMM_WORLD, rc);
-	}
-	
-	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-	MPI_Comm_size(MPI_COMM_WORLD, &nproc);
+    // Allocating buffer for processor name
+    char name[MPI_MAX_PROCESSOR_NAME];
+    
+    // Initialising MPI with return code
+    int returnCode = MPI_Init(&argc, &argv);
+    
+    // Check for initialising error and abort
+    if (returnCode != MPI_SUCCESS) 
+    {
+        printf ("Error starting MPI program\n");
+        MPI_Abort(MPI_COMM_WORLD, returnCode);
+    }
 
-	if (myrank == 0) 
-	{
-		printf("main reports %d procs\n", nproc);
-	}
-	
-	namelen = MPI_MAX_PROCESSOR_NAME;
-	
-	MPI_Get_processor_name(name, &namelen);
-	printf("hello world %d from ’%s’\n", myrank, name);
-	
-	/* implicit barrier in Finalize */
-	/*MPI_Barrier(MPI_COMM_WORLD);*/
-	MPI_Finalize();
-	
-	return 0;
+    int myrank; // rank of mpi process
+    int nproc; // number of processors
+    
+    
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank); // allocate space for rank
+    MPI_Comm_size(MPI_COMM_WORLD, &nproc); // allocate space for number of processor
+
+    if (myrank == 0) // if the main process
+    {
+        printf("main reports %d procs\n", nproc);
+    }
+    
+    // for the communicator world put a barrier
+    // this ensures that main reports is printed first
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    // name length allocated
+    int namelen = MPI_MAX_PROCESSOR_NAME;
+    MPI_Get_processor_name(name, &namelen);
+    
+    // print rank and name of process
+    printf("hello world %d from ’%s’\n", myrank, name);
+    
+    // Finalize
+    MPI_Finalize();
+    
+    return 0;
 }
